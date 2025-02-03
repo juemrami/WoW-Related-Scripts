b93e490dd927031095532a20403c5f11109d7d9c
Julio Ramirez <juemrami@gmail.com>
2025-02-02 18:25:16 -0800

    Update guildInviteInPlayerDropDown.lua
    - migrate to new api `Menu.ModifyMenu`
    - address the #noscript changes to add friend api

--- guildInviteInPlayerDropDown.lua
+++ guildInviteInPlayerDropDown.lua
@@ -7,29 +7,21 @@ aura_env.onEvent = function(event)
 end
 aura_env.hookDropDrown = function()
     if aura_env.init then return end
-    DropDownList1:HookScript("OnShow", function(self)
-        if self.dropdown:GetName() == "FriendsDropDown"
-            and self.dropdown.name
-        then
-            UIDropDownMenu_AddButton({
-                text = "Guild Invite",
-                func = function() GuildInvite(self.dropdown.name) end,
-                notCheckable = true,
-                tooltipTitle = "Guild Invite",
-                tooltipText = "Invite this player to your guild",
-                -- tooltipOnButton = true,
-            }, 1)
-            UIDropDownMenu_AddButton({
-                text = "Add Friend",
-                func = function()
-                    C_FriendList.AddFriend(self.dropdown.name)
-                end,
-                notCheckable = true,
-                tooltipTitle = "Add Friend",
-                tooltipText = "Add Player to Friends list",
-                -- tooltipOnButton = true,
-            }, 1)
+    local realm = GetNormalizedRealmName()
+    Menu.ModifyMenu("MENU_UNIT_FRIEND", function(frame, description, context)
+        if not IsInGuild() then return end
+        description:CreateDivider();
+        local normalizedName = context.name
+        if context.server and (context.server ~= realm) then
+            normalizedName = normalizedName.."-"..context.server
         end
+        description:CreateButton("Guild Invite", function()
+           C_GuildInfo.Invite(normalizedName)
+        end)
+        -- disabled. cant call AddFriend from inside a WA anymore
+        -- description:CreateButton("Add Friend", function()
+        --     C_FriendList.AddFriend(normalizedName)
+        -- end)
     end)
     aura_env.init = true
 end
